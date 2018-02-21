ec0c4539da8857a08fc628816debaaf6cf6f2681
fresto32 <tajpereira@gmail.com>
2018-02-21 14:53:18 +1030

    Begun recursive file finder

---
+++ src/recurs_file_finder.cc
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include "boost/filesystem.hpp"
+
+namespace fs = std::filesystem;
+
+int main() {
+  fs::create_directories("/Users/fresto32/Dropbox/[8] Computer Science/" + 
+      "lines_tracker/src/");
+  for (auto& p: fs::recursive_directory_iterator("a")) std::cout << p << "\n";
+
+  return 0;
+}
+
